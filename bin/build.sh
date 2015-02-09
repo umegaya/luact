b40fda2021b41969c7d1a67fa7d21d942242eb8e
Takehiro Iyatomi <iyatomi@gmail.com>
2015-02-09 17:00:06 +0900

    add image gc to docker build script

--- bin/build.sh
+++ bin/build.sh
@@ -7,3 +7,6 @@ cp luact/lxc/docker/Dockerfile.$TYPE ./Dockerfile
 docker build --no-cache=true -t umegaya/luact:$TYPE .
 rm Dockerfile
 docker push umegaya/luact:$TYPE
+
+# remove unused image
+docker rmi $(docker images | grep "^<none>" | awk "{print \$3}")
